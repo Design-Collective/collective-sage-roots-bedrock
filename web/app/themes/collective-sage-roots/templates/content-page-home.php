6934f5b698587f7855910a14894179297ea6691f
sugarskull <rodrigo@designcollective.io>
2015-06-11 21:22:01 -0700

    offset for parallax and fixes for background; stellar

--- web/app/themes/collective-sage-roots/templates/content-page-home.php
+++ web/app/themes/collective-sage-roots/templates/content-page-home.php
@@ -5,7 +5,15 @@
 
 ?>
 
-<section id="slider" class="section parallax nomargin notopborder dark"style="transform: translate(0px, 0px); background: url(https://placeholdit.imgix.net/~text?txtsize=113&txt=1500%C3%971000&w=1500&h=1000) no-repeat; background-size: cover; background-position:center 200px;" data-height-lg="670" data-height-md="500" data-height-sm="490" data-height-xs="450" data-height-xxs="200"  data-stellar-background-ratio="-0.3">
+<section id="slider" class="section parallax nomargin notopborder dark" 
+    data-height-lg="auto" 
+    data-height-md="auto" 
+    data-height-sm="auto" 
+    data-height-xs="auto" 
+    data-height-xxs="auto"  
+    data-stellar-background-ratio="0.1" 
+    data-stellar-horizontal-offset="0"
+    data-stellar-vertical-offset="0">
     <div class="overlay in"></div>
     <div class="container clearfix overlayed-container">
         <div class="vertical-middle center" style="opacity: 2.22222222222222; position: absolute; top: 50%; width: 100%; margin-top: -14px;">
